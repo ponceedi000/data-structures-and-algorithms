715788de2f34ef9febdcb32b30a70486c38b0266
Ediberto Ponce <ediberto01@gmail.com>
2021-12-18 17:12:57 -0800

    fixed file structure, created/tested 3 new methods inside my LinkedList class

---
+++ python/code_challenges/linked_list/linked_list.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, value, next =None):
+        self.value = value
+        self.next = next
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert(self, value):
+        node = Node(value)
+        if self.head is not None:
+            node.next = self.head
+        self.head = node
+
+    def includes(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
---
+++ python/tests/test_linked_list.py
@@ -0,0 +1,32 @@
+from code_challenges.linked_list.linked_list import LinkedList, Node
+import pytest
+
+def test_node_instance():
+    node = Node(1, None)
+    assert node.next == None
+    assert node.value == 1
+
+def test_node_instance_fail():
+    node = Node(1, None)
+    assert node.next != node
+    assert node.value != 2
+
+def test_linked_list():
+    node = Node(1, None)
+    ll = LinkedList(node)
+    assert ll.head == node
+
+def test_includes_node_value():
+    ll = LinkedList()
+    ll.insert("one")
+    ll.insert("two")
+    ll.insert("three")
+    ll.insert("four")
+    ll.insert("five")
+    ll.insert("six")
+
+    assert ll.includes("six") == True
+
+    node = Node(1, None)
+    ll = LinkedList(node)
+    assert ll.head == node
